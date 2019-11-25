8fc399408b985eeaec11b6bb14b641e1d1041695
Cheng-Yu Chiang <43870330+chychiang@users.noreply.github.com>
2019-11-24 22:51:02 -0800

    added comments

--- application.py
+++ application.py
@@ -5,10 +5,11 @@ from random import random
 from time import sleep
 from threading import Thread, Event
 
+# for data parsing
 import csv
 
+# dependencies for ftp to work
 from ftplib import FTP  # documentation: https://docs.python.org/3/library/ftplib.html
-import time
 
 
 __author__ = 'slynn'
@@ -24,47 +25,45 @@ socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
 thread = Thread()
 thread_stop_event = Event()
 
-# config
+# ftp config
 piHost = '192.168.43.196'  # ip of the pi
 ftpServerAccount: str = 'pi'
 ftpServerPassword: str = 'raspberry'
 
 
-def randomNumberGenerator():
+def sendDataWeb():
     """
-    every 1 second and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
+    opens a ftp connection with the pi, downloads the data.txt file
+    parses the data using csv reader and emits
     """
-    number = 0
-    temp = [0]
-    #infinite loop of magical random numbers
-    #print("Making random numbers")
     while not thread_stop_event.isSet():
         # opens the connection
-    # all upload/download should be made within this with statement
-        ftp = FTP(piHost)
-        ftp.login(ftpServerAccount, ftpServerPassword)
-        print(ftp.getwelcome())
-        ftp.cwd('/files')
-        ftp.dir()
+        # all upload/download should be made within this with statement
+        ftp = FTP(piHost)   # create the ftp object with the ip of the pi
+        ftp.login(ftpServerAccount, ftpServerPassword)  # login pi 
+        ftp.cwd('/files')   # change dir /pi/ftp/files - only have permission in /files
+        ftp.dir()   # displays the dir on python console
         with open ('data.txt', 'wb') as fp:
+            # fetches the file from pi and save it as "data.txt" locally
             ftp.retrbinary('RETR ' + 'data.txt', fp.write)
-        #number = round(random()*10, 3)
-        #print(number)
         with open('data.txt') as csv_file:
-            temp = []
+            # parses the data from "data.txt"
+            temp = []   # generates a new array every loop
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 for item in row:
-                    temp.append(item)
-            number1 = temp[0]
+                    temp.append(item)   # stores item in data.txt into array temp
+            number1 = temp[0]   # temporary
             number2 = temp[1]
-            print(temp)
-            
+            # TODO: reorginize data as nested arrays
+            # print(temp)
+
+        # TODO: send multiply "strands" of data with different keys 
         socketio.emit('newnumber', {'number': "Machine1: "+ number1 + " Machine2:"+ number2}, namespace='/test')
-        socketio.sleep(1)
+        socketio.sleep(1)   # wait 1 sec
 
 
+#ben - these we didn't touch
 @app.route('/')
 def index():
     #only by sending this page first will the client be connected to the socketio instance
@@ -79,7 +78,7 @@ def test_connect():
     #Start the random number generator thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
-        thread = socketio.start_background_task(randomNumberGenerator)
+        thread = socketio.start_background_task(sendDataWeb)
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
